012892ab41545e62f2ff64153b6b6652e8881c48
Jonatan Ezequiel Barbuto Beran <test@fakemail.com>
2021-10-01 20:05:34 -0300

    TP2_Listo

---
+++ TP2BarbutoJonatan/Entidades/Ciclomotor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        /// <summary>
+        /// Constructor de clase que llama al constructor de la clase base
+        /// </summary>
+        /// <param name="marca"> Parametro marca </param>
+        /// <param name="chasis"> Parametro chasis </param>
+        /// <param name="color"> Parametro color </param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Genera un Stringbuilder y lo completa (Titulo, informacion de la clase base y el tamanio)
+        /// </summary>
+        /// <returns> El objeto StringBuilder convertido a String </returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2BarbutoJonatan/Entidades/Sedan.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo 
+        { 
+            CuatroPuertas, CincoPuertas 
+        }
+        private ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"> Parametro marca </param>
+        /// <param name="chasis"> Parametro chasis </param>
+        /// <param name="color"> Parametro color </param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            this.tipo = ETipo.CuatroPuertas;
+        }
+
+        /// <summary>
+        /// Constructor de clase que llama al constructor de la clase base
+        /// </summary>
+        /// <param name="marca"> Parametro marca </param>
+        /// <param name="chasis"> Parametro chasis </param>
+        /// <param name="color"> Parametro color </param>
+        /// <param name="tipo"> Parametro tipo </param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+            :base(chasis, marca, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Genera un Stringbuilder y lo completa (Titulo, informacion de la clase base y el tamanio)
+        /// </summary>
+        /// <returns> El objeto StringBuilder convertido a String </returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}TIPO : {this.tipo}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
